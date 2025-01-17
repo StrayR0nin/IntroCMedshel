4350fe0dfe4702b6d0fcaea3a4887eff3a3c0e87
StrayR0nin <194439288+StrayR0nin@users.noreply.github.com>
2025-01-17 10:46:24 -0500

    Assignemnt 1-1

---
+++ PersonalInfo.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+
+char name[] = "Robert Hall";
+
+printf("%s\n",name);
+printf("2/27/1949\n");
+printf("work 000-444-0101\n");
+printf("Cell 000-444-0189\n");
+
+}
