4c24bce0ee7928530a034885a466de2d64389371
StrayR0nin <194439288+StrayR0nin@users.noreply.github.com>
2025-02-02 10:40:50 -0500

    Assignment 2-2

---
+++ Hurricane.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main() {
+    
+    int windSpeed;
+    printf("How fast is the wind speed in mph?\n");
+    scanf("%d, %s", &windSpeed); 
+
+    if (windSpeed < 74) {
+        printf("Not a Hurricane ");
+    }
+
+    if (windSpeed < 96 && windSpeed >= 74) {
+        printf("Category One Hurricane "); 
+    }
+
+    if (windSpeed < 111 && windSpeed >= 96) {
+        printf("Category Two Hurricane ");
+    }
+
+    if (windSpeed < 130 && windSpeed >= 111) {
+        printf("Category Three Hurricane ");
+    }
+
+    if (windSpeed < 157 && windSpeed >= 130) {
+        printf("Category Four Hurricane ");
+    }
+
+    if (windSpeed >= 157) {
+        printf("Category Five Hurricane");
+    }
+ 
+}
