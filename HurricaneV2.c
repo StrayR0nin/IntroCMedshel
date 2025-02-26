28380cc936a8879faa7945e99b95509542bb5622
StrayR0nin <194439288+StrayR0nin@users.noreply.github.com>
2025-02-26 16:43:25 -0500

    Assignment 3-2

---
+++ HurricaneV2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+int windSpeed; 
+int hurricaneCat(int windSpeed);
+
+int main() {
+    hurricaneCat(175.99);
+}
+
+int hurricaneCat(int windSpeed) {
+
+    if (windSpeed < 74) {
+        printf("Not a Hurricane ");
+    }
+
+    if (windSpeed < 96 && windSpeed >= 74) {
+        printf("Category One Hurricane "); 
+    }
+
+    if (windSpeed < 111 && windSpeed >= 96) {
+        printf("Category Two Hurricane ");
+    }
+
+    if (windSpeed < 130 && windSpeed >= 111) {
+        printf("Category Three Hurricane ");
+    }
+
+    if (windSpeed < 157 && windSpeed >= 130) {
+        printf("Category Four Hurricane ");
+    }
+
+    if (windSpeed >= 157) {
+        printf("Category Five Hurricane");
+    }
+}
