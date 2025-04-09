77e45f833596a4a7223672cfe726d982a6130168
StrayR0nin <194439288+StrayR0nin@users.noreply.github.com>
2025-04-09 15:05:30 -0400

    Assignment 4-1

---
+++ HomeSalesV2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(
+  
+    char salesperson[]; = {"Danielle", "Edward", "Francis", "Z"};
+    char sp; 
+    char spInitial[]; = {"D", "E", "F"};
+    int sale[];
+    int total[];
+    
+    printf("Enter the name of the Salespersons\n");
+    scanf("%c", &sp); 
+    
+    while (sp != "Z") {
+        
+    
+        switch (salesperson[]) {
+
+            case "Danielle"
+            
+            printf("How much did they make?\n");
+            scanf("%i", &sale[0]);
+            printf("Sale:"sale[0]);
+        }
+
+    }
+
+)
